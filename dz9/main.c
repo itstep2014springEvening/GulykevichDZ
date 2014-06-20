91f19da632a9c87e62fe2fb9c0f8d61542ae8f91
Moril <ivan.gulykevich@gmail.com>
2014-06-20 20:25:03 +0300

    dz9

---
+++ dz9/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+    int a,b,c,d=0;
+    printf ("Please, enter the number A\n");
+    scanf ("%d", &a);
+    //printf("%d/2=%d\n",a,a/2);
+    b=a/2;
+    //printf("%d-%d=%d\n",a,b,a-b);
+    c=a-b;
+    if (d<b,d++) {
+        printf ("1,");
+    }
+    if (d<c,d++) {
+        printf ("2,");
+    }
+    return 0;
+ }
+

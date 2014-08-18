36e3f618498bcb7037f34dba1ab52918f11524cf
Moril <ivan.gulykevich@gmail.com>
2014-08-18 21:04:58 +0300

    one file version

--- DZ16/1Selection/main.c
+++ DZ16/1Selection/main.c
@@ -3,60 +3,48 @@
 #include <math.h>
 #define n 5
 
-void output(int a[], int size);
+void sort(int a[], int size);
 void input(int a[], int size);
+void output(const int a[], int size);
 
 int main()
 {
     int a[n];
-    int i, j;
-    int buf;
-    int min;
-     printf("Enter the elements of array \n");
-    for(i=0; i<n; i++){
-        scanf("%d", &a[i]);
-    }
-    input (a,n);  /*вызываем функцию которая найдет min элемент, поменяем местами a [min] и a[i], и выведет фунцию=)*/
+    input (a,n);
+    output (a,n);
+    sort  (a,n);
     output (a,n);
     return 0;
 }
-void output(int a[], int size)
+
+void output(const int a[], int size)
 {
-    int i, j;
-    int buf;
-    int min;
-for(i = 0; i < n-1; i++) {
-    min=i;
-        for(j = i+1; j < n; j++)
-            if(a[j] < a[min])
-                min = j;
-        buf=a[i];
-        a[i]=a[min];
-        a[min]=buf;
-    }
-     for(i=0; i<n; i++)
+    for(int i=0; i<n; i++)
+    {
         printf("%d ", a[i]);
-
+    }
     printf("\n");
-
 }
 
 void input(int a[], int size)
 {
-    int i, j;
-    int buf;
+    printf("Enter the elements of array \n");
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+}
+void  sort  (int a[], int size)
+{
     int min;
-for(i = 0; i < n-1; i++) {
-    min=i;
-        for(j = i+1; j < n; j++)
+    for(int i = 0; i < n-1; i++)
+    {
+        min=i;
+        for(int j = i+1; j < n; j++)
             if(a[j] < a[min])
                 min = j;
-        buf=a[i];
+        int buf=a[i];
         a[i]=a[min];
         a[min]=buf;
     }
-     for(i=0; i<n; i++)
-        printf("%d ", a[i]);
-
-    printf("\n");
 }
